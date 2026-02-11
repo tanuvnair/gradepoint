767da9ac61a46de33878e54600215c8e1fca68c1
Tanuv Nair <tanuvnair@gmail.com>
2026-02-12 00:30:24 +0530

    feat: removed template code

--- src/app.tsx
+++ src/app.tsx
@@ -1,29 +1,15 @@
-import { type RouteDefinition, Router } from "@solidjs/router";
+import { Router } from "@solidjs/router";
 import { FileRoutes } from "@solidjs/start/router";
 import { MetaProvider } from "@solidjs/meta";
 import { Suspense } from "solid-js";
-import { querySession } from "./auth";
-import Auth from "./components/Context";
-import Nav from "./components/Nav";
-import ErrorNotification from "./components/Error";
 import "./app.css";
-
-export const route: RouteDefinition = {
-  preload: ({ location }) => querySession(location.pathname),
-};
 
 export default function App() {
   return (
     <Router
       root={(props) => (
         <MetaProvider>
-          <Auth>
-            <Suspense>
-              <Nav />
-              {props.children}
-              <ErrorNotification />
-            </Suspense>
-          </Auth>
+          <Suspense>{props.children}</Suspense>
         </MetaProvider>
       )}
     >
